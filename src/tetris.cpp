3cfc4bb092d42d85c11aa319beb1f0270c97c5d6
Lane <zhanglintc623@gmail.com>
2015-06-04 18:36:37 +0800

    src folder & tetris.cpp

---
+++ src/tetris.cpp
@@ -0,0 +1,18 @@
+﻿/*
+ Main process here.  http://zhanglintc.co
+(C) COPYRIGHT 2015-06-04 ZhangLin. Nothing reserved.
+*/
+
+#include "tetris.h"
+
+/*******************************************************
+Function: main entrance
+Argument: none
+Return  : int
+*******************************************************/
+int main() {
+    //here we go !!!
+    return 0;
+}
+
+
