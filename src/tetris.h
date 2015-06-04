71388b1bbeeebc54bca21d60aba81f9aa61c2d5c
Lane <zhanglintc623@gmail.com>
2015-06-04 20:57:01 +0800

    add tetris.h

---
+++ src/tetris.h
@@ -0,0 +1,28 @@
+﻿/*
+ All the defines and declares.   http://zhanglintc.co
+(C) COPYRIGHT 2014-6-04 zhanglin. Nothing reserved.
+*/
+
+#ifndef _TETRIS_H_
+#define _TETRIS_H_
+
+// includes
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+// defines
+#define CTRL_UP         72
+#define CTRL_DOWN       80
+#define CTRL_RIGHT      77
+#define CTRL_LEFT       75
+#define CTRL_SPACE      32
+#define CTRL_ESC        27
+#define CTRL_ENTER      13
+#define CTRL_F5         63
+
+// declares
+
+#endif //_TETRIS_H_
+
